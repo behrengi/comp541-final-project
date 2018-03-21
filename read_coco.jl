c5866b8c29603b02b63491eeeaed71635398049a
Bihter Akyol <bakyol17@ku.edu.tr>
2018-03-22 02:05:46 +0300

    Read JSON files and turned them into a read-ready format for Read.jl.

--- read_coco.jl
+++ read_coco.jl
@@ -1,5 +1,35 @@
+include("Read.jl")
+
+import Read
 using JSON
+using PyCall
 
+@pyimport nltk
 dir = "/Users/bihterakyol/Desktop/SPRING2018/COMP541/datasets/COCO/"
+text_dir = string(dir, "annotations/")
+train_image_dir = string(dir, "train2014")
+val_image_dir = string(dir, "test2014") # val2014???? downloaded wrong image data
 
-raw_captions = JSON.parsefile(string(dir, "annotations/captions_train2014.json"))
+######################################
+# Annotations needs some preprocessing
+# ready to be nltk-split with simple
+# space-split.
+######################################
+raw_JSON_train = JSON.parsefile(string(text_dir, "captions_train2014.json"))
+raw_JSON_val = JSON.parsefile(string(text_dir, "captions_val2014.json"))
+raw_JSON = merge(raw_JSON_train, raw_JSON_val)
+
+raw_captions = []
+tokens = Dict()
+for image in values(raw_JSON["images"])
+    tokens[image["id"]] = (image["file_name"], Any[])
+end
+
+for annotation in values(raw_JSON["annotations"])
+    push!(tokens[annotation["image_id"]][2], split(lowercase(annotation["caption"])))
+end
+tokens = Dict(collect(values(tokens)))
+
+i2w, wi2 = Read.CreateDictionaries(tokens)
+# train = Read.ImageTokens(train_image_dir, raw_training, tokens)
+# val = Read.ImageTokens(val_image_dir, raw_dev, tokens)
