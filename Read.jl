643119de249586d2857d9704f5e007b6f05ce95c
Bihter Akyol <bakyol17@ku.edu.tr>
2018-03-26 01:12:52 +0300

    Tweaks on Read.jl Also added JoinWithSpaces.

--- Read.jl
+++ Read.jl
@@ -3,14 +3,14 @@ module Read
 using Images
 using StatsBase: countmap  
 
-"""
-data: Image matrix
-tokens: Tokens array, each entry has a String array with sentence tokens.
-"""
-struct CaptionedImage
-    data
-    tokens
-end
+# """
+# data: Image matrix
+# tokens: Tokens array, each entry has a String array with sentence tokens.
+# """
+# struct CaptionedImage
+#     data
+#     tokens
+# end
 
 """
 
@@ -27,7 +27,7 @@ function TokensDict(raw_captions)
     tokens = Dict()
     for i in 1:size(raw_captions, 1)
         filename, _ = String.(split(raw_captions[i, 1], '#'))
-        if !(filename in keys(tokens))
+        if !haskey(tokens, filename)
             tokens[filename] = Any[]
         end
         push!(tokens[filename], String.(split(lowercase(raw_captions[i, 2]))))
@@ -36,24 +36,41 @@ function TokensDict(raw_captions)
 end
 
 
-"""
-tokens: a dict where keys are the image files and values are the tokenized sentences. 
-Returns an array of CaptionImages. 
-"""
-function ImageTokens(tokens, image_dir)
-    images = CaptionedImage[]
+# """
+# tokens: a dict where keys are the image files and values are the tokenized sentences. 
+# Returns an array of CaptionImages. 
+# """
+# function ImageTokens(tokens, image_dir)
+#     images = CaptionedImage[]
 
-    for filename in keys(tokens)
-        ############################
-        # Images need preprocessing?
-        # Check the paper
-        ############################
-        push!(images, CaptionedImage(
-            load(string(image_dir, filename)),
-            tokens[filename])
-            )
+#     for filename in keys(tokens)
+#         ############################
+#         # Images need preprocessing?
+#         # Check the paper
+#         ############################
+#         push!(images, CaptionedImage(
+#             load(string(image_dir, filename)),
+#             tokens[filename])
+#             )
+#     end
+#     return(images)
+# end
+
+"""
+Loads the images and returns a dictionary where keys are the filenames and the
+values are the images.
+"""
+function ImageDict(image_dir, filenames)
+    ImageDict = Dict()
+    i = 1
+    for file in filenames
+        ImageDict[file] = Images.load(string(image_dir, file))
+        i = i + 1
+        if i % 200 == 0
+            println("image loading process: %", 100 * (i / length(filenames)))
+        end
     end
-    return(images)
+    return(ImageDict)
 end
 
 """
@@ -108,44 +125,33 @@ end
 caption_length: Dict(length1 => [index, [imagefilename, caption; imagefilename; caption, ...]])
 index: index of the next minibatch item. 
 """
-function CaptionLengthDict(captions)
-    caption_length_dict = Dict()
-    index = 1
+function CaptionCountDict(captions)
+    caption_count = Dict()
     for (filename, sentences) in captions
-        for sentence in sentences
+        for sentence in sentences 
             sentence_length = length(sentence)
             n_input = reshape([filename, sentence], 1, 2) # new input
-            if sentence_length in keys(caption_length_dict)
-                caption_length_dict[sentence_length][2] = [caption_length_dict[sentence_length][2]; n_input]
+            if haskey(caption_count, sentence_length)
+                caption_count[sentence_length] = [caption_count[sentence_length]; n_input]
             else
-                caption_length_dict[sentence_length] = [1, n_input]
+                caption_count[sentence_length] = n_input
             end
         end
     end 
-    return(caption_length_dict)
+    return(caption_count)
 end
-
-# function BLEU(hypot_tokens, candid_tokens; n = 4)
-#     h, c = length(hypot_tokens), length(candid_tokens)
-#     bp_penalty = c >= h ? 1 : exp(1 - (h / c)) # brevity penalty
-#     bleu = 1
-#     for i in 1:n
-#         bleu *= BLEU(i, hypot_tokens, candid_tokens)
-#     end
-#     println(isnan(bp_penalty * (bleu .^ (1 / n))))
-#     return(bp_penalty * (bleu .^ (1 / n)))
-# end
-
-# function BLEU(n, hypot_tokens, candid_tokens)
-#     hypot_ngrams = Ngram(hypot_tokens, n)
-#     candid_ngrams = Ngram(candid_tokens, n)
-#     precision = Precision(hypot_ngrams, candid_ngrams)
-#     return(precision)
-# end
 
 function FlattenTokens(tokens_dict)
     tokens = values(tokens_dict)
     return(collect(Iterators.flatten(Iterators.flatten(tokens))))
+end
+
+function JoinWithSpaces(tokens)
+    tokens_with_spaces = String[]
+    for token in tokens
+        push!(tokens_with_spaces, token, " ")
+    end
+    return(join(tokens_with_spaces[1:end-1]))
 end
 
 end
