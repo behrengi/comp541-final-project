4dde6a06d81d65f146a58a8325fa470792e39ec6
Bihter Akyol <bakyol17@ku.edu.tr>
2018-04-09 02:14:54 +0300

    Functions for loading vgg layers, feature extraction

--- Read.jl
+++ Read.jl
@@ -1,16 +1,18 @@
+for p in ("Images","StatsBase","MAT","JLD","Knet")
+    Pkg.installed(p) == nothing && Pkg.add(p)
+end
+
 module Read
 
 using Images
-using StatsBase: countmap  
+using StatsBase: countmap
+using MAT  
+using JLD
+using Knet
 
-# """
-# data: Image matrix
-# tokens: Tokens array, each entry has a String array with sentence tokens.
-# """
-# struct CaptionedImage
-#     data
-#     tokens
-# end
+ftype = Float32
+atype = gpu() >= 0 ? KnetArray{ftype} : Array{ftype}
+
 
 """
 
@@ -23,14 +25,19 @@ filename1 -> [Sentence1["token1", "token2", ...], Sentence2["token1", "token2", 
 .
 .
 """
-function TokensDict(raw_captions)
+function TokensDict(raw_captions; start_token=false)
     tokens = Dict()
     for i in 1:size(raw_captions, 1)
-        filename, _ = String.(split(raw_captions[i, 1], '#'))
+        filename = String(split(raw_captions[i, 1], '#')[1])
         if !haskey(tokens, filename)
             tokens[filename] = Any[]
         end
-        push!(tokens[filename], String.(split(lowercase(raw_captions[i, 2]))))
+        splitted = String.(split(lowercase(raw_captions[i, 2])))
+        if start_token
+            push!(tokens[filename], ["_BOS_"; splitted; "_EOS_"])
+        else
+            push!(tokens[filename], [splitted; "_EOS_"])
+        end
     end
     return(tokens)
 end
@@ -60,17 +67,37 @@ end
 Loads the images and returns a dictionary where keys are the filenames and the
 values are the images.
 """
-function ImageDict(image_dir, filenames)
-    ImageDict = Dict()
-    i = 1
-    for file in filenames
-        ImageDict[file] = Images.load(string(image_dir, file))
-        i = i + 1
-        if i % 200 == 0
-            println("image loading process: %", 100 * (i / length(filenames)))
+function FeatureDict(image_dir, filenames)
+    image_dict = Dict()
+    feature_file = joinpath(image_dir, "features.jld") 
+    if !isfile(feature_file)
+        feature_dict = Dict()
+        vgg_layers = VggLoad()["layers"][1:35]
+        i = 1  
+        largest = 0
+        smallest = 0
+        for file in filenames
+            features = VggFeatures(Images.load(joinpath(image_dir, file)), vgg_layers)
+
+            largest = max(maximum(features), largest)
+            smallest = min(minimum(features), smallest)
+
+            feature_dict[file] = features     
+            i = i + 1
+            if i % 200 == 0
+                println("image loading process: %", 100 * (i / length(filenames)))
+            end
         end
+
+        for file in filenames
+            feature_dict[file] = (feature_dict[file] - smallest) / (largest - smallest)
+        end
+
+        JLD.save(feature_file, "feature_dict", feature_dict)
+    else
+        feature_dict = JLD.load(feature_file)["feature_dict"]
     end
-    return(ImageDict)
+    return(feature_dict)
 end
 
 """
@@ -84,6 +111,7 @@ function CreateDictionaries(tokens; minfreq = 5)
     tokencounts = filter((t, f) -> f>=minfreq, tokencounts)
     i2w = collect(keys(tokencounts))
     push!(i2w, "_UNK_")
+    push!(i2w, "_BOS_")
     w2i = Dict(zip(i2w, 1:length(i2w)))
     return(i2w, w2i)
 end
@@ -98,7 +126,7 @@ function Ngram(tokens, n)
     end
     for i in 1:(length(tokens) - n + 1)
         from, to = i, i + n - 1
-        push!(ngrams, convert(Array{String}, tokens[from:to]))
+        push!(ngrams, tokens[from:to])
     end
     return(ngrams)
 end
@@ -122,23 +150,23 @@ function NgramDict(tokens_dict, n)
 end
 
 """
-caption_length: Dict(length1 => [index, [imagefilename, caption; imagefilename; caption, ...]])
+caption_length: Dict(length1 => [(imagefilename, caption), (imagefilename, caption), ...])
 index: index of the next minibatch item. 
 """
 function CaptionCountDict(captions)
-    caption_count = Dict()
+    captions_by_length = Dict()
     for (filename, sentences) in captions
         for sentence in sentences 
             sentence_length = length(sentence)
-            n_input = reshape([filename, sentence], 1, 2) # new input
-            if haskey(caption_count, sentence_length)
-                caption_count[sentence_length] = [caption_count[sentence_length]; n_input]
+            n_input = (filename, sentence) # new input
+            if haskey(captions_by_length, sentence_length)
+                captions_by_length[sentence_length] = [captions_by_length[sentence_length]; n_input]
             else
-                caption_count[sentence_length] = n_input
+                captions_by_length[sentence_length] = [n_input]
             end
         end
     end 
-    return(caption_count)
+    return(captions_by_length)
 end
 
 function FlattenTokens(tokens_dict)
@@ -152,6 +180,60 @@ function JoinWithSpaces(tokens)
         push!(tokens_with_spaces, token, " ")
     end
     return(join(tokens_with_spaces[1:end-1]))
+end
+
+function VggLoad()
+    file = joinpath(pwd(), "data", "vgg_19.mat")
+    if !isdir("data")
+        mkdir(joinpath(pwd(), "data"))
+    end
+    if !isfile(file)
+        download("http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat", file)
+    end
+    return(matread(file))
+end
+
+function PreProcessImage(image; image_size=224)
+    image = permutedims(float32.(rawview(channelview(image))), (2, 3, 1))
+    h, w = size(image, 1), size(image, 2)
+    if h < w
+        resized = imresize(image, (256, floor(Int, w * (256 / h))))
+    else
+        resized = imresize(image, (floor(Int, h * (256 / w)), 256))
+    end
+    im_center = floor.(Int, center(resized))
+    im_size_halved = Int(image_size / 2)
+    cropped = resized[(im_center[1] - im_size_halved) : (im_center[1] + im_size_halved - 1), 
+                        (im_center[2] - im_size_halved) : (im_center[2] +  im_size_halved - 1), :]
+    return(atype(cropped))
+    
+end
+
+function VggFeatures(input_dir, vgg_layers)
+    
+    input = Read.PreProcessImage(input_dir)
+    input = atype(reshape(input, (size(input)..., 1)))
+    for layer in vgg_layers
+        if layer["type"] == "conv" || layer["type"] == "pool"
+            padding = Int.(layer["pad"][1])
+            stride = Int.(layer["stride"][1])
+            if layer["type"] == "conv"
+                weights = atype(layer["weights"][1])
+                input = conv4(weights, input; padding=padding, stride=stride)
+            else
+                input = pool(input; padding=padding, stride=stride, window = Int.(layer["pool"]))
+            end
+        elseif layer["type"] == "relu"
+            input = relu.(input)
+        end
+    end
+    return(reshape(input, (size(input, 1) * size(input, 2), size(input, 3))))
+end
+
+function OneHot(i, size)
+    onehot = spzeros(1, size)
+    onehot[i] = 1
+    return(onehot)
 end
 
 end
